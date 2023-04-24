f2001be474f81095be478aa8f8b544a7d9b37402
harishtpj <harishtpj@hotmail.com>
2023-04-24 11:44:25 +0530

    Added Preprocessor Code

--- com/harishlangs/hcl/Hcl.java
+++ com/harishlangs/hcl/Hcl.java
@@ -1,12 +1,20 @@
 package com.harishlangs.hcl;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
+import org.python.core.PyList;
+import org.python.core.PyObject;
+import org.python.core.PyString;
+import org.python.core.PyTuple;
+import org.python.util.PythonInterpreter;
 
 
 public class Hcl {
@@ -25,11 +33,35 @@ public class Hcl {
     }
 
     private static void runFile(String path) throws IOException {
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
-        run(new String(bytes, Charset.defaultCharset()));
+        ClassLoader classLoader = Hcl.class.getClassLoader();
+        URL classLocation = classLoader.getResource("com/harishlangs/hcl/Hcl.class");
+        String classPath = classLocation.getPath();
+        String packagePath = classPath.replace("Hcl.class", "");
+        String pyFilePath = packagePath + "preprocessor/proc.py";
 
-        if (hadError) System.exit(65);
-        if (hadRuntimeError) System.exit(70);
+        try (PythonInterpreter pyInterp = new PythonInterpreter()) {
+            pyInterp.execfile(pyFilePath);
+            PyObject preprocess = pyInterp.get("preprocess");
+            PyObject preprocessPrgm = preprocess.__call__(new PyString(path));
+            PyTuple res = (PyTuple) preprocessPrgm.__tojava__(PyTuple.class);
+
+            PyList imports = (PyList) res.pyget(0).__tojava__(PyList.class);
+            String proc_src = res.pyget(1).asString();
+
+            
+            for (int i = 0; i < imports.__len__(); i++) {
+                String imported = imports.__getitem__(i).asString();
+                run(imported);
+
+                if (hadError) System.exit(65);
+                if (hadRuntimeError) System.exit(70);
+            }
+            
+            run(proc_src);
+
+            if (hadError) System.exit(65);
+            if (hadRuntimeError) System.exit(70);
+        }
     }
 
     private static void runPrompt() throws IOException {
